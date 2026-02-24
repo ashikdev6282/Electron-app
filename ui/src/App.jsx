b0bff1b81ba350f5bdc21485c4e623ea9e928599
ashikdev6282 <ashiknandanan@gmail.com>
2026-02-24 13:21:35 +0530

    changes in app.jsx

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,28 +1,3 @@
-import { useState } from "react";
-import Dictate from "./pages/Dictate";
-import MyDictations from "./pages/MyDictations";
-import Settings from "./pages/Settings";
-import BottomNav from "./components/BottomNav";
-
 export default function App() {
-  const [tab, setTab] = useState("dictate");
-  const [dictations, setDictations] = useState([]);
-
-  return (
-    <div className="h-full bg-black">
-      <div className="pb-16">
-        {tab === "dictate" && (
-          <Dictate onSaveDictation={setDictations} />
-        )}
-
-        {tab === "list" && (
-          <MyDictations dictations={dictations} />
-        )}
-
-        {tab === "settings" && <Settings />}
-      </div>
-
-      <BottomNav tab={tab} setTab={setTab} />
-    </div>
-  );
+  return null;
 }
